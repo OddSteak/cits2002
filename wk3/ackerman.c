3db87ad34f13a118dee14eaf07b836961655dfc9
sedentaryowl <sedentaryowl@riseup.net>
2024-08-05 12:01:16 +0800

    no global for depth

--- wk3/ackerman.c
+++ wk3/ackerman.c
@@ -2,36 +2,41 @@
 #include <stdlib.h>
 
 int count = 0;
-int max_depth = 0;
-int aman(int m, int n, int depth);
+int *aman(int m, int n, int depth);
 
 int main(int ac, char **av)
 {
     if (ac != 3)
         printf("idiot\n");
 
-    printf("result is %d\n", aman(atoi(av[1]), atoi(av[2]), 1));
+    int *res = aman(atoi(av[1]), atoi(av[2]), 1);
+    printf("value is %d\n", res[0]);
+    printf("depth is %d\n", res[1]);
     printf("count is %d\n", count);
-    printf("depth is %d\n", max_depth);
 }
 
-int aman(int m, int n, int depth)
+int *aman(int m, int n, int depth)
 {
-    if (depth > max_depth)
-        max_depth = depth;
     count++;
+
+    int *res = (int *)malloc(2 * sizeof(int));
     if (m == 0) {
-        return n+1;
+        res[0] = n + 1;
+        res[1] = depth;
     }
     else if (m > 0 && n == 0) {
-        return aman(m - 1, 1, depth+1);
+        res = aman(m -1 , 1, depth + 1);
     }
     else if (m > 0 && n > 0) {
-        return aman(m - 1, aman(m, n-1, depth + 1), depth + 1);
+        int *res1, *res2;
+        res1 = aman(m, n-1, depth + 1);
+        res2 = aman(m-1, res1[0], depth + 1);
+        res[0] = res2[0];
+        res[1] = res1[1] > res2[1] ? res1[1] : res2[1];
     }
     else
         fprintf(stderr, "function is not defined for negative values\n");
 
-    return 0;
+    return res;
 }
 
