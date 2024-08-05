60a46f01b8d2f69e13ae6b0a56ef3fbf9ac5169e
sedentaryowl <sedentaryowl@riseup.net>
2024-08-05 21:56:11 +0800

    Reapply "no global count"
    This reverts commit 0b58fc850c9391205a520ce17ad0f3414b6c0212.

--- wk3/ackerman.c
+++ wk3/ackerman.c
@@ -1,45 +1,45 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int count = 0;
-int *aman(int m, int n, int depth);
+int* aman(int m, int n, int depth, int count);
 
-int main(int ac, char **av)
+int main(int ac, char** av)
 {
     // count: 2432
     // depth: 63
     // value: 61
-    if (ac != 3)
+    if (ac != 3) {
         printf("idiot\n");
+        return 1;
+    }
 
-    int *res = aman(atoi(av[1]), atoi(av[2]), 1);
+    int* res = aman(atoi(av[1]), atoi(av[2]), 1, 1);
     printf("value is %d\n", res[0]);
     printf("depth is %d\n", res[1]);
-    printf("count is %d\n", count);
+    printf("count is %d\n", res[2]);
 }
 
-int *aman(int m, int n, int depth)
+int* aman(int m, int n, int depth, int count)
 {
-    count++;
+    int* res = (int*)malloc(3 * sizeof(int));
 
-    int *res = (int *)malloc(2 * sizeof(int));
     if (m == 0) {
         res[0] = n + 1;
         res[1] = depth;
-    }
-    else if (m > 0 && n == 0) {
-        res = aman(m -1 , 1, depth + 1);
-    }
-    else if (m > 0 && n > 0) {
+        res[2] = count;
+    } else if (m > 0 && n == 0) {
+        res = aman(m - 1, 1, depth + 1, count+1);
+    } else if (m > 0 && n > 0) {
         int *res1, *res2;
-        res1 = aman(m, n-1, depth + 1);
-        res2 = aman(m-1, res1[0], depth + 1);
+        res1 = aman(m, n - 1, depth + 1, count+1);
+        res2 = aman(m - 1, res1[0], depth + 1, count+1);
         res[0] = res2[0];
         res[1] = res1[1] > res2[1] ? res1[1] : res2[1];
-    }
-    else
+        res[2] = res1[2] + res2[2];
+    } else {
         fprintf(stderr, "function is not defined for negative values\n");
+        exit(EXIT_FAILURE);
+    }
 
     return res;
 }
-
