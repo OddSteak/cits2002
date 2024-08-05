b3ce98ca99bc6a567d844f9fe7a3f237c8b9493e
sedentaryowl <sedentaryowl@riseup.net>
2024-08-05 23:46:55 +0800

    dead simple romans

---
+++ wk3/roman.c
@@ -0,0 +1,67 @@
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+char romans[] = { 'M', 'D', 'C', 'L', 'X', 'V', 'I' };
+int reps[] = { 1000, 500, 100, 50, 10, 5, 1 };
+
+void romanize(int num, char* res);
+
+int main(int ac, char** av)
+{
+    if (ac != 2) {
+        printf("idiot\n");
+        exit(EXIT_FAILURE);
+    }
+
+    int num = atoi(av[1]);
+    for (int i = 1; i < num + 1; i++) {
+        char *buf = (char *)malloc(1000);
+
+        romanize(i, buf);
+        buf = realloc(buf, strlen(buf) + 1);
+
+        printf("%d: %s\n", i, buf);
+    }
+}
+
+void romanize(int num, char* res)
+{
+    strcpy(res, "");
+    int i = 0;
+    while (num >= 10) {
+        if (num >= reps[i]) {
+            num -= reps[i];
+            strncat(res, &romans[i], 1);
+        } else {
+            i++;
+        }
+    }
+
+    if (num == 9) {
+        strcat(res, "IX");
+        return;
+    }
+
+    if (num < 4) {
+        for (int i = 0; i < num; i++) {
+            strncat(res, &romans[6], 1);
+        }
+        return;
+    }
+
+    if (num == 4) {
+        strcat(res, "IV");
+        return;
+    }
+
+    if (num >= 5) {
+        strncat(res, &romans[5], 1);
+        num -= 5;
+
+        for (int i = 0; i < num; i++) {
+            strncat(res, &romans[6], 1);
+        }
+        return;
+    }
+}
